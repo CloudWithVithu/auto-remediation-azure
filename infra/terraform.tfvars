552251f8c95a421372f9af80411cda6e884e0906
CloudWithVithu <vithushanvisuvalingam@gmail.com>
2025-07-27 06:12:51 +0530

    Terrafrom infra get configured

---
+++ infra/terraform.tfvars
@@ -0,0 +1,3 @@
+location            = "eastasia"
+resource_group_name = "autoheal-rg"
+vm_name             = "autoheal-vm"
---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  type    = string
+  default = "eastasia"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "autoheal-rg"
+}
+
+variable "vm_name" {
+  type    = string
+  default = "autoheal-vm"
+}
